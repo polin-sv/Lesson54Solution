2094d04b456b373980b76a2324550479707cde39
polin-sv <polinasvetlay@icloud.com>
2026-04-08 17:01:22 +0300

    soft task09

--- Task09Project/logic.cpp
+++ Task09Project/logic.cpp
@@ -4,8 +4,23 @@
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму цифр заданного числа, кратных 5.
 
+
+
 #include "logic.h"
 
 int sum(int number) {
-	return 0;
+	number = number < 0 ? -number : number;
+
+	if (number == 0) {
+		return 0;
+	}
+
+	int last_digit = number % 10;
+	int digit = 0;
+
+	if (last_digit % 5 == 0) {
+		digit = last_digit;
+	}
+
+	return digit + sum(number / 10);
 }
